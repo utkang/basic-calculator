2ba3c04902a8b4f2e6c1f240212d904e21615a37
utkangencdev <utkangencdev@gmail.com>
2023-09-01 00:14:12 +0300

    Create some functions for calculating, and imply them event listener. Then fix some mistakes.

--- public/script.js
+++ public/script.js
@@ -1,17 +1,26 @@
 "use strict";
 
+//* Create variables to hold the `display` element and the `buttons` element
 const display = document.querySelector("#display");
 const buttons = document.querySelector(".button");
 
+//* Create a `displayValue` variable to hold the string value of the input that will be displayed on the calculator screen
 let displayValue = "0";
 
+//* Create variables to hold the `firstOperand`, `operator`, and `waitingForSecondOperand` values
+let firstOperand = null;
+let operator = null;
+let waitingForSecondOperand = false;
+
+//* Initialize the display
 updateDisplay();
 
+//todo Create a function to update the display
 function updateDisplay() {
   display.value = displayValue;
 }
 
-// Add an event listener to the `buttons` element, which will delegate. 
+//todo Add an event listener to the `buttons` element, which will delegate. 
 // That means that if a button is clicked, the click event will bubble up to the `buttons` element, 
 buttons.addEventListener("click", function (event) {
 
@@ -22,7 +31,10 @@ buttons.addEventListener("click", function (event) {
   }
 
   if (element.classList.contains("operator")) {
-    console.log("operator", element.value);
+    // console.log("operator", element.value);
+    handleOperator(element.value);
+    updateDisplay();
+
     return;
   }
 
@@ -41,12 +53,6 @@ buttons.addEventListener("click", function (event) {
     
     return;
   }
-
-  if (element.classList.contains("calculate")) {
-    
-    console.log("calculate", element.value);
-    return;
-  }
   
   console.log("number", element.value);
 
@@ -54,18 +60,80 @@ buttons.addEventListener("click", function (event) {
   updateDisplay();
 });
 
-function inputNumber(number) {
+//todo Create a function to handle the operators
+function handleOperator(nextOperator) {
   
-  // If the `displayValue` is '0', replace it with the `number` pressed
-  if (displayValue === "0") {
-    displayValue = number;
+  const inputValue = parseFloat(displayValue);
+
+  if(operator && waitingForSecondOperand) {
+    
+    operator = nextOperator;
+    return;
   }
-  // Otherwise append the `number` pressed to the `displayValue`
-  else {
-    displayValue += number;
+
+  // Verify that `firstOperand` is null and that the `inputValue` is not a `NaN` value
+  if (firstOperand === null) {
+    firstOperand = inputValue;
+
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+    
+    // Update the `displayValue` to the `result`
+    displayValue = `${parseFloat(result.toFixed(7))}`;
+    firstOperand = result;
   }
+
+  // Update the existing `operator` variable
+  waitingForSecondOperand = true;
+  operator = nextOperator;
+
+  console.log(displayValue, firstOperand, operator, waitingForSecondOperand);
 }
 
+//todo Create a function to perform the calculation
+function calculate(firstOperand, secondOperand, operator) {
+
+  // Perform the calculation based on the operator
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+//todo Create a function to handle the number inputs
+function inputNumber(number) {
+  
+  if (waitingForSecondOperand === true) {
+    
+    displayValue = number;
+    waitingForSecondOperand = false;
+
+  } else {
+    
+    // If the `displayValue` is '0', replace it with the `number` pressed
+    if (displayValue === "0") {
+      displayValue = number;
+    }
+    // Otherwise append the `number` pressed to the `displayValue`
+    else {
+      displayValue += number;
+    }
+  }
+
+  console.log(displayValue, firstOperand, operator, waitingForSecondOperand);
+}
+
+//todo Create a function to handle the decimal point
 function inputDecimal(dot) {
   // If the `displayValue` does not contain a decimal point
   if (!displayValue.includes(dot)) {
@@ -73,6 +141,7 @@ function inputDecimal(dot) {
   }
 }
 
+//todo Create a function to clear the calculator
 function clear() {
   // Reset the `displayValue` to '0'
   displayValue = "0";
